4258db0075e6ad772bf6b0a613ac1991035cea42
Kemal Hatunoğlu <kemalhatunoglu@gmail.com>
2022-02-03 16:44:03 +0300

    NoContent çıkarıldı.

--- src/rentACar/Application/Features/Cars/Commends/DeleteCar/DeleteCarCommand.cs
+++ src/rentACar/Application/Features/Cars/Commends/DeleteCar/DeleteCarCommand.cs
@@ -5,11 +5,11 @@ using MediatR;
 
 namespace Application.Features.Cars.Commends.DeleteCar
 {
-    public class DeleteCarCommand : IRequest<NoContent>
+    public class DeleteCarCommand : IRequest<IResult>
     {
         public int Id { get; set; }
 
-        public class DeleteCarCommandHandler : IRequestHandler<DeleteCarCommand, NoContent>
+        public class DeleteCarCommandHandler : IRequestHandler<DeleteCarCommand, IResult>
         {
             private readonly ICarRepository _carRepository;
 
@@ -18,15 +18,15 @@ namespace Application.Features.Cars.Commends.DeleteCar
                 _carRepository = carRepository;
             }
 
-            public async Task<NoContent> Handle(DeleteCarCommand request, CancellationToken cancellationToken)
+            public async Task<IResult> Handle(DeleteCarCommand request, CancellationToken cancellationToken)
             {
                 var deleteCar = await _carRepository.GetAsync(c => c.Id == request.Id);
                 if (deleteCar != null)
                 {
                     await _carRepository.DeleteAsync(deleteCar);
-                    return new NoContent();
+                    return new SuccessResult();
                 }
-                return new NoContent();
+                return new ErrorResult();
             }
         }
     }
